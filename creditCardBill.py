6a058709b8079fd2c95ea1f4049565546b8d1a4b
HybridRbt <littlejack0120@gmail.com>
2014-06-22 16:34:15 -0500

    add prob 3

--- creditCardBill.py
+++ creditCardBill.py
@@ -168,4 +168,92 @@ def find_lowest_payment(initial_balance, annual_interest_rate):
 # balance = 3926
 # annualInterestRate = 0.2
 
-print "Lowest Payment: " + str(find_lowest_payment(balance, annualInterestRate))
+#print "Lowest Payment: " + str(find_lowest_payment(balance, annualInterestRate))
+
+"""
+PROBLEM 3: USING BISECTION SEARCH TO MAKE THE PROGRAM FASTER (25 points possible)
+You'll notice that in Problem 2, your monthly payment had to be a multiple of $10. Why did we make it that way?
+You can try running your code locally so that the payment can be any dollar and cent amount (in other words, the
+monthly payment is a multiple of $0.01). Does your code still work? It should, but you may notice that your code
+runs more slowly, especially in cases with very large balances and interest rates. (Note: when your code is running
+on our servers, there are limits on the amount of computing time each submission is allowed, so your observations
+from running this experiment on the grading system might be limited to an error message complaining about too
+much time taken.)
+
+Well then, how can we calculate a more accurate fixed monthly payment than we did in Problem 2 without running into
+the problem of slow code? We can make this program run faster using a technique introduced in lecture - bisection
+search!
+
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+To recap the problem: we are searching for the smallest monthly payment such that we can pay off the entire balance
+within a year. What is a reasonable lower bound for this payment value? $0 is the obvious answer, but you can do
+better than that. If there was no interest, the debt can be paid off by monthly payments of one-twelfth of the
+original balance, so we must pay at least this much every month. One-twelfth of the original balance is a good lower
+bound.
+
+What is a good upper bound? Imagine that instead of paying monthly, we paid off the entire balance at the end of the
+year. What we ultimately pay must be greater than what we would've paid in monthly installments, because the interest
+was compounded on the balance we didn't pay off each month. So a good upper bound for the monthly payment would be
+one-twelfth of the balance, after having its interest compounded monthly for an entire year.
+
+In short:
+
+Monthly interest rate = (Annual interest rate) / 12.0
+Monthly payment lower bound = Balance / 12
+Monthly payment upper bound = (Balance x (1 + Monthly interest rate)^12) / 12.0
+
+Write a program that uses these bounds and bisection search (for more info check out the Wikipedia page on bisection
+search) to find the smallest monthly payment to the cent (no more multiples of $10) such that we can pay off the debt
+within a year. Try it out with large inputs, and notice how fast it is (try the same large inputs in your solution to
+Problem 2 to compare!). Produce the same return value as you did in Problem 2.
+
+Note that if you do not use bisection search, your code will not run - your code only has 30 seconds to run on our
+servers.
+
+"""
+
+
+def get_monthly_lowest_payment(monthly_lowest_payment_low_limit, monthly_lowest_payment_up_limit):
+    result = (monthly_lowest_payment_up_limit + monthly_lowest_payment_low_limit) / 2
+    return result
+
+
+def find_lowest_payment_bisec(initial_balance, annual_interest_rate):
+    # define precision
+    eps = 0.001
+
+    # set initial limits
+    monthly_lowest_payment_low_limit = initial_balance / 12
+    monthly_lowest_payment_up_limit = initial_balance * (1 + annual_interest_rate / 12) ** 12 / 12
+
+    monthly_lowest_payment_initial_up_limit = monthly_lowest_payment_up_limit
+    monthly_lowest_payment = 0
+
+    # when total paid is less than balance
+    while monthly_lowest_payment < monthly_lowest_payment_initial_up_limit:
+        remain_balance = initial_balance
+        monthly_lowest_payment = get_monthly_lowest_payment(monthly_lowest_payment_low_limit,
+                                                            monthly_lowest_payment_up_limit)
+        remain_balance = yearly_balance(remain_balance, annual_interest_rate, monthly_lowest_payment)
+        if 0 < remain_balance < eps:
+            break
+        elif remain_balance < 0:
+            # paid too much each month
+            monthly_lowest_payment_up_limit = monthly_lowest_payment
+        else:
+            # paid too little
+            monthly_lowest_payment_low_limit = monthly_lowest_payment
+
+    return monthly_lowest_payment
+
+
+#Test Case 1:
+balance = 320000
+annualInterestRate = 0.2
+
+print "Lowest Payment: " + str(find_lowest_payment_bisec(balance, annualInterestRate))
