3ef8be1f6ce9f3a5edac1ec518f6f5f6b98f77da
HybridRbt <littlejack0120@gmail.com>
2014-06-22 13:26:46 -0500

    initial commit

---
+++ countingBob.py
@@ -0,0 +1,16 @@
+__author__ = 'jeredyang'
+
+s = 'bdjgbobobobjoboboobobpphodo'
+
+# convert s to be all lower case and setup counter
+s = s.lower()
+count = 0
+
+# for every three characters check if a bob is in
+for index in range(len(s)):
+    temp_s = s[index: index + 3]
+    if temp_s == "bob":
+        count += 1
+
+# print result
+print "Number of times bob occurs is: " + str(count)
